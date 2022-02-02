abc5195b3797dd67ee05ae065ffe537b12f0d7ec
Deepak Agarwal <agarwaldeepak491@gmail.com>
2022-02-02 12:33:19 +0530

    Sunrise and sunset time issue and date and time issue resolved

--- script.js
+++ script.js
@@ -1,29 +1,30 @@
-const input = document.querySelector('input'),
-    search = document.getElementById('search_btn'),
-    infoTxt = document.getElementById('message'),
-    locationBtn = document.getElementById('locationBtn'),
-    temp_field = document.getElementById('temp'),
-    sunrise_field = document.getElementById('sunrise'),
-    sunset_field = document.getElementById('sunset'),
-    city_name = document.getElementById('city_name'),
-    country_name = document.getElementById('country_name'),
-    description_field = document.getElementById('description'),
-    real_feel_value = document.getElementById('real_feel_value'),
-    humidity_value = document.getElementById('humidity_value'),
-    weather_image = document.getElementById('weather_img');
+const input = document.querySelector("input"),
+    search = document.getElementById("search_btn"),
+    infoTxt = document.getElementById("message"),
+    locationBtn = document.getElementById("locationBtn"),
+    temp_field = document.getElementById("temp"),
+    sunrise_field = document.getElementById("sunrise"),
+    sunset_field = document.getElementById("sunset"),
+    city_name = document.getElementById("city_name"),
+    country_name = document.getElementById("country_name"),
+    description_field = document.getElementById("description"),
+    real_feel_value = document.getElementById("real_feel_value"),
+    humidity_value = document.getElementById("humidity_value"),
+    weather_image = document.getElementById("weather_img");
+var tz=0;
 let api;
 var appid = "a68ca9034e611cf04b44696858a0f726";
 // Fetching Cookie Value and storing the value to city_cookie
 let city_cookie = "";
 try {
     // document.cookie = "city=; expires=Thu, 01 Jan 1970 00:00:00 UTC; path=/;";
-    city_cookie = getCookie('weather_city');
+    city_cookie = getCookie("weather_city");
 } catch (error) {
     console.error(error);
 }
 
-// Calling the requestApi() function if enter key is pressed while typing in the input field 
-input.addEventListener("keyup", e => {
+// Calling the requestApi() function if enter key is pressed while typing in the input field
+input.addEventListener("keyup", (e) => {
     if (e.key == "Enter" && input.value != "") {
         requestApi(input.value);
     }
@@ -40,16 +41,15 @@ search.addEventListener("click", () => {
 locationBtn.addEventListener("click", () => {
     if (navigator.geolocation) {
         navigator.geolocation.getCurrentPosition(onSuccess, onError);
-        infoTxt.innerText = 'Locating Your Location';
+        infoTxt.innerText = "Locating Your Location";
         infoTxt.classList.add("valid");
     } else {
-        infoTxt.innerText = 'Unsupported Browser';
+        infoTxt.innerText = "Unsupported Browser";
         infoTxt.classList.add("invalid");
     }
 });
 
-
-// Setting the api 
+// Setting the api
 function requestApi(city) {
     api = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric&appid=${appid}`;
     fetchData();
@@ -72,10 +72,13 @@ function onError(error) {
 function fetchData() {
     infoTxt.innerText = "Fetching weather details...";
     infoTxt.classList.add("valid");
-    fetch(api).then(res => res.json()).then(result => weatherDetails(result)).catch(() => {
-        infoTxt.innerText = "Something went wrong";
-        infoTxt.classList.replace("valid", "invalid");
-    });
+    fetch(api)
+        .then((res) => res.json())
+        .then((result) => weatherDetails(result))
+        .catch(() => {
+            infoTxt.innerText = "Something went wrong";
+            infoTxt.classList.replace("valid", "invalid");
+        });
 }
 
 // Assigning the fetched values to the fields
@@ -86,8 +89,9 @@ function weatherDetails(info) {
     } else {
         const city = info.name;
         const country = info.sys.country;
-        const sunrise = unixToDate(info.sys.sunrise);
-        const sunset = unixToDate(info.sys.sunset);
+        const sunrise = unixToDate(info.sys.sunrise, info.timezone);
+        const sunset = unixToDate(info.sys.sunset, info.timezone);
+        tz=info.timezone
         const { description, id, icon } = info.weather[0];
         const { temp, feels_like, humidity } = info.main;
         const len = icon.length;
@@ -140,7 +144,16 @@ function weatherDetails(info) {
             weather_image.width = "140";
         }
 
-        console.log(city, country, description, temp, feels_like, humidity, sunrise, sunset);
+        console.log(
+            city,
+            country,
+            description,
+            temp,
+            feels_like,
+            humidity,
+            sunrise,
+            sunset
+        );
         city_name.innerText = city;
         country_name.innerText = country;
         description_field.innerText = description;
@@ -157,25 +170,29 @@ function weatherDetails(info) {
 }
 
 // Converting the unix time to time stamp
-function unixToDate(unix) {
-    const date = new Date(unix * 1000);
-    const hours = date.getHours();
-    const minutes = "0" + date.getMinutes();
-    if (hours < 10) {
-        return `0${hours}:${date.getMinutes()}`;
-    } else {
-        return `${hours}:${minutes.substr(-2)}`;
-    }
+function unixToDate(unix, timezone) {
+    dateObj = new Date((unix + (timezone)) * 1000);
+    hours = dateObj.getUTCHours();
+    minutes = dateObj.getUTCMinutes();
+    seconds = dateObj.getUTCSeconds();
+    minutes = (seconds<30) ? minutes : minutes+1;
+    hours = (minutes<59) ?  hours : hours+1 ;
+    minutes = (minutes<59) ?  minutes : minutes-60 ;
+    formattedTime =
+        hours.toString().padStart(2, "0") +
+        ":" +
+        minutes.toString().padStart(2, "0")
+    return formattedTime;
 }
 
 // function to set the cookie value
 function getCookie(cname) {
     let name = cname + "=";
     let decodedCookie = decodeURIComponent(document.cookie);
-    let ca = decodedCookie.split(';');
+    let ca = decodedCookie.split(";");
     for (let i = 0; i < ca.length; i++) {
         let c = ca[i];
-        while (c.charAt(0) == ' ') {
+        while (c.charAt(0) == " ") {
             c = c.substring(1);
         }
         if (c.indexOf(name) == 0) {
@@ -188,12 +205,10 @@ function getCookie(cname) {
 // function to set the cookie value
 function setCookie(cvalue) {
     const d = new Date();
-    d.setTime(d.getTime() + (30 * 24 * 60 * 60 * 1000));
+    d.setTime(d.getTime() + 30 * 24 * 60 * 60 * 1000);
     let expires = "expires=" + d.toUTCString();
     document.cookie = "weather_city=" + cvalue + ";" + expires + ";path=/";
 }
-
-
 
 console.log(city_cookie); // prints the cookie value to the console.
 /* 
@@ -203,5 +218,57 @@ It is executed only when the page is loaded.
 if (city_cookie.length > 0) {
     requestApi(city_cookie);
 } else {
-    requestApi('New Delhi');
+    requestApi("New Delhi");
 }
+
+function getDateTime() {
+    //new Date(new Date().toUTCString()) 
+    var dateObj = new Date(new Date().toUTCString()).getTime();
+    var now = new Date(((dateObj/1000) + (tz)) * 1000);
+    var year = now.getUTCFullYear();
+    var month = now.getUTCMonth();
+    var day = now.getUTCDate();
+    var hour = now.getUTCHours();
+    var minute = now.getUTCMinutes();
+    var second = now.getUTCSeconds();
+    var am_pm = "";
+    if (day.toString().length == 1) {
+        day = '0' + day;
+    }
+    if (hour > 12) {
+        hour = hour - 12;
+        am_pm = "PM";
+    } else {
+        am_pm = "AM";
+    }
+    if (hour.toString().length == 1) {
+        hour = '0' + hour;
+    }
+    if (minute.toString().length == 1) {
+        minute = '0' + minute;
+    }
+    if (second.toString().length == 1) {
+        second = '0' + second;
+    }
+    const monthNames = ["Jan", "Feb", "Mar", "Apr", "May", "Jun",
+        "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"
+    ];
+    const weekday = new Array(7);
+    weekday[0] = "Sun";
+    weekday[1] = "Mon";
+    weekday[2] = "Tue";
+    weekday[3] = "Wed";
+    weekday[4] = "Thu";
+    weekday[5] = "Fri";
+    weekday[6] = "Sat";
+
+    let day_name = weekday[now.getUTCDay()];
+    var dateTime = day_name + ' | ' + day + ' ' + monthNames[month] + ' ' + year + ' | ' + hour + ':' + minute + ':' + second + ' ' + am_pm;
+    return dateTime;
+}
+
+
+setInterval(function () {
+    currentTime = getDateTime();
+    document.getElementById("time").innerHTML = currentTime;
+}, 1000);
